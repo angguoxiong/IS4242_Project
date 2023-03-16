6ce41ef4fbfaa1c759396ce7a60d2b9c42642d07
Guo <anggx2440@gmail.com>
2023-03-16 21:17:17 +0800

    added comments

--- ML Models/Neural Network/neural_network.py
+++ ML Models/Neural Network/neural_network.py
@@ -6,8 +6,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 
-x_train = pd.DataFrame()
-y_train = pd.DataFrame()
+x_train = pd.DataFrame()    # genre, description, duration, directors, stars, votes, user rating, movie rating
+y_train = pd.DataFrame()    # ratings
 x_test = pd.DataFrame()
 
 def build_nn_model(num_neurons_1, num_neurons_2):
@@ -23,3 +23,6 @@ nn = build_nn_model(30, 30)
 nn_history = nn.fit(x_train, y_train, batch_size=5, epochs=5, verbose=1)
 y_pred_nn_prob = nn.predict(x_test)
 y_pred_nn = np.argmax(y_pred_nn_prob, axis=1)
+
+
+### tuning for hyperparameters
