bbdee6f38c1fe427bef412533d8af11d771f08ab
sthitiacharya <sthitiacharya@gmail.com>
2023-03-16 22:28:25 +0800

    added hyperparameter tuning

--- ML Models/Neural Network/neural_network.py
+++ ML Models/Neural Network/neural_network.py
@@ -5,10 +5,15 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
+#Tuning
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import accuracy_score
 
 x_train = pd.DataFrame()    # genre, description, duration, directors, stars, votes, user rating, movie rating
 y_train = pd.DataFrame()    # ratings
 x_test = pd.DataFrame()
+y_test = pd.DataFrame()
 
 def build_nn_model(num_neurons_1, num_neurons_2):
     nn = Sequential()
@@ -26,3 +31,18 @@ y_pred_nn = np.argmax(y_pred_nn_prob, axis=1)
 
 
 ### tuning for hyperparameters
+model = KerasClassifier(build_fn=build_nn_model)
+
+param_grid = {'activation': ['relu', 'softmax'],
+              'optimizer': ['adam', 'sgd'],
+              'batch_size': [5, 10, 20],
+              'epochs': [5, 10, 20]}
+
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
+grid_search.fit(x_train, y_train)
+
+optimal_clf = grid_search.best_estimator_
+
+Y_pred = optimal_clf.predict(x_test)
+acc_after_tuning = accuracy_score(y_test, Y_pred)
+print("test accuracy = {:.2%}".format(acc_after_tuning))
