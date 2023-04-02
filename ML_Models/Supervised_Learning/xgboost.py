0820bcc8ced21c685d77b65d28e8a3d61b8d2248
xueqi <72065687+dengxueqi@users.noreply.github.com>
2023-04-02 17:36:33 +0800

    added deepfm and updated xgboost .py files

--- ML_Models/Supervised_Learning/xgboost.py
+++ ML_Models/Supervised_Learning/xgboost.py
@@ -1,21 +1,21 @@
-import pandas as pd
-import numpy as np
-import pickle
+import math
 
 # Models
 import xgboost as xgb
 
 #Tuning and Cross Validation
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 
 import warnings
+
+from helper_functions import compress_pickle, decompress_pickle
+
 warnings.filterwarnings("ignore")
 
 
 def build_xgboost_model(verbosity):
-    xgboost = xgb.XGBRegressor(verbosity)
+    xgboost = xgb.XGBRegressor(verbosity=verbosity)
     return xgboost
 
 
@@ -32,10 +32,10 @@ def tune_xgboost_hyperparameter_with_cross_validation(x_train, y_train):
     estimator = GridSearchCV(xgb_tuning, parameters, cv=3)
     estimator.fit(x_train, y_train)
     tuned_xgb = estimator.best_estimator_
-    compress_pickle('../../Data Files/Model Files/', 'xgb', tuned_xgb)
+    compress_pickle('../../Data_Files/Model_Files/', 'xgb', tuned_xgb)
 
 def evaluate_xgboost(x_test, y_test):
-    optimal_xgb = decompress_pickle('../../Data Files/Model Files/', 'xgb') 
+    optimal_xgb = decompress_pickle('../../Data_Files/Model_Files/', 'xgb')
 
     y_pred = optimal_xgb.predict(x_test)
     mae = mean_absolute_error(y_test, y_pred)
